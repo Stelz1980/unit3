c69adfd67dbc2ada179ea2184921eb3d706a44f2
Stelz1980 <84735649+Stelz1980@users.noreply.github.com>
2022-08-15 22:40:16 +0300

    test123

--- src/main/java/ru/gb/lim/task1/Executor.java
+++ src/main/java/ru/gb/lim/task1/Executor.java
@@ -3,7 +3,7 @@ package ru.gb.lim.task1;
 public class Executor {
 
     public static void main(String[] args) {
-        System.out.println("Рввasfgfggsgassф");
+        System.out.println("Рввasfgfgjhjgsgassф");
     }
 
 }
